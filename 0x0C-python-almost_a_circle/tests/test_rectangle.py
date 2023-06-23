4224d827d0df9a9ee50770e169e8deb6adce68b9
Chirindo <jessicahchimweri@gmail.com>
2023-06-23 14:26:34 -0700

    test

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -9,10 +9,7 @@ and the number of the test for that task
 import unittest
 import io
 import contextlib
-from models.base import Base
 from models.rectangle import Rectangle
-from models.square import Square
-
 
 class TestRectangle(unittest.TestCase):
     """Test cases for the Rectangle class."""
