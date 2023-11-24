6150686c6c0f28e89bb961e5b4fe2502bc946b2b
chirindo <jessicahchimweri@gmail.com>
2023-11-24 01:59:17 +0000

    task 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,23 +1,31 @@
 #!/usr/bin/python3
-"""adds the State object “Louisiana”
-to the database hbtn_0e_6_usa"""
+"""
+This module fethces all states
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
 
-    import sys
-    from model_state import Base, State
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2],
-                                   sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-    session = Session(engine)
-    state = session.query(State).filter(State.name == sys.argv[4]).first()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state_name = sys.argv[4]
+    state = session.query(State).filter_by(name=state_name).first()
+
     if state:
-        print("{}".format(state.id))
+        print(state.id)
     else:
         print("Not found")
+
     session.close()
