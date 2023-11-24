4b212b7a475dee8097fd59542d6736254dcb5b87
chirindo <jessicahchimweri@gmail.com>
2023-11-24 01:38:08 +0000

    task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
-"""takes in an argument and displays all values
-in the states table of hbtn_0e_0_usa
-where name matches the argument
-and is safe from SQL injections"""
+"""
+This module lists state based on user input
+"""
+import MySQLdb
+import sys
 
-if __name__ == '__main__':
-
-    import MySQLdb
-    import sys
-
-    db = MySQLdb.connect(host='localhost', port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name=%s\
-                ORDER BY states.id ASC", (sys.argv[4],))
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
+    query = "SELECT * FROM states \
+             WHERE name LIKE BINARY %s \
+             ORDER BY states.id"
+    cur.execute(query, (sys.argv[4],))
+
+    states = cur.fetchall()
+    for state in states:
+        print(state)
